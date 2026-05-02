4d0f57a0c06749a625b810664cb3348ce4190704
Rijo Sebasitan <rijo.sebastian@burrowsolutions.com>
2026-05-02 17:56:38 +0530

    Mastering Layouts: _ViewStart, _ViewImports, and Tag Helpers in .NET 10

--- WebApplication1/Controllers/StudentController.cs
+++ WebApplication1/Controllers/StudentController.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Model;
 
-namespace WebApplication1.Controllers
+namespace MVCApp.Controllers
 {
     public class StudentController : Controller
     {
         public IActionResult Index()
-        //{
-        //    var students = new List<string> { "Rijo", "Sebasitan" };
-        //    return View(students);
-        //}
         {
             var students = new List<Student>
-       {
-           new Student { Name = "Rijo" },
-            new Student { Name = "Sebasitan" }
-
-       };
+            {
+                new Student { Name= "John"},
+                new Student { Name = "Sara"}
+            };
             return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
         }
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
